9cb190b5c095145ca728fa8cfef1ced4feb97c32
Charles-André Després <cadespres@outlook.com>
2021-11-23 11:46:45 -0500

    After rewrite:run

--- src/main/java/org/springframework/samples/petclinic/rewrite/SchoolConfig.java
+++ src/main/java/org/springframework/samples/petclinic/rewrite/SchoolConfig.java
@@ -1,38 +1,54 @@
 package org.springframework.samples.petclinic.rewrite;
 
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SchoolConfig {
-        /** Purposely misaligned to cause auto-formatting.
-         * {@link}
-         * {@link DogSchool
-         * {@link DogSchool()}
-         * {@link
-         *      DogSchool}
-         */
+    /** Purposely misaligned to cause auto-formatting.
+     * {@link}
+     * {@link DogSchool
+     * {@link DogSchool()}
+     * {@link
+     *      DogSchool}
+     */
     @Bean
-    public DogSchool dogSchool() {
+    DogSchool dogSchool() {
         return new DogSchool();
     }
 
-        /**
-         * <a href="
-         * https://...html">
-         * label</a>.
-         */
+    /**
+     * <a href="
+     * https://...html">
+     * label</a>.
+     */
     @Bean
-    public CatSchool catSchool() {
+    CatSchool catSchool() {
         return new CatSchool();
     }
 
-        /**
-         */
+    /**
+     */
     @Bean
-    @ConditionalOnBean({CatSchool.class, DogSchool.class})
-    public VetSchool vetSchool() {
+    @Conditional(ConditionCatSchoolOrDogSchool.class)
+    VetSchool vetSchool() {
         return new VetSchool();
+    }
+
+    private static class ConditionCatSchoolOrDogSchool extends AnyNestedCondition {
+        ConditionCatSchoolOrDogSchool() {
+            super(ConfigurationPhase.REGISTER_BEAN);
+        }
+
+        @ConditionalOnBean(CatSchool.class)
+        class CatSchoolCondition {
+        }
+
+        @ConditionalOnBean(DogSchool.class)
+        class DogSchoolCondition {
+        }
     }
 }
